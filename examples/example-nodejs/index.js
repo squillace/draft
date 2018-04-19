1f898af6ea14bfc6535ee8431112cf0692de0a5b
Ralph Squillace <ralph@squillace.com>
2018-04-18 21:49:03 -0700

    modified for debug

--- examples/example-nodejs/index.js
+++ examples/example-nodejs/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end("Hello World, I'm Node.js!");
+  response.end("Hello World, I'm Node.js on AKS!");
 }
 
 const server = http.createServer(requestHandler);
