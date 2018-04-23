356a4bc2a65a7bcb93ea399cddf14616c660ab7d
Ralph Squillace <ralph@squillace.com>
2018-04-22 22:48:28 -0700

    setting up example of working debug with svcat

--- examples/example-nodejs/index.js
+++ examples/example-nodejs/index.js
@@ -3,7 +3,14 @@ const port = process.env.PORT || 8080;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end("Hello World, I'm Node.js on AKS!");
+  response.end("Hello World, I'm Node.js on AKS in WestEurope! here's mongo " + process.env.MONGODB_URI);
+  // To use service mongo, we added a number of environment variables
+// to your application, as listed below:
+// MONGO_CONNECTIONSTRING
+// MONGO_HOST
+// MONGO_PASSWORD
+// MONGO_PORT
+// MONGO_USERNAME
 }
 
 const server = http.createServer(requestHandler);
