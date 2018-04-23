8f818f65bc07637d373ce09881a8f598f1f0555e
Ralph Squillace <ralph@squillace.com>
2018-04-23 15:51:35 -0700

    figuring this out temporarily

--- examples/example-nodejs/index.js
+++ examples/example-nodejs/index.js
@@ -3,7 +3,6 @@ const port = process.env.PORT || 8080;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end("Hello World, I'm Node.js on AKS in WestEurope! here's mongo " + process.env.MONGODB_URI);
   // To use service mongo, we added a number of environment variables
 // to your application, as listed below:
 // MONGO_CONNECTIONSTRING
@@ -11,6 +10,7 @@ const requestHandler = (request, response) => {
 // MONGO_PASSWORD
 // MONGO_PORT
 // MONGO_USERNAME
+  response.end("Hello World, I'm Node.js on AKS in WestEurope! here's mongo " + process.env.MONGO_CONNECTIONSTRING);
 }
 
 const server = http.createServer(requestHandler);
